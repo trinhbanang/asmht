8afb5c533dd4badec488214bf8d548cef94c834a
trinhbanang <nangtbph07510@fpt.edu.vn>
2020-03-31 17:47:04 +0700

    4 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ,
+              '../css/global.css',
+              '../css/easy-responsive-tabs.css',
+              '../css/slider.css',
+              '../css/style.css',]
 })
 export class AppComponent  {
   name = 'Angular';
