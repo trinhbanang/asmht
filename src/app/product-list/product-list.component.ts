cbe122f30aaacb95c9b5c360b9c1d88d956b6853
trinhbanang <nangtbph07510@fpt.edu.vn>
2020-04-04 21:14:38 +0700

    3 files modified, 3 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css',
+              '../css/global.css',
+              '../css/easy-responsive-tabs.css',
+              '../css/slider.css',
+              '../css/style.css',]
 })
 export class ProductListComponent implements OnInit {
 
--- src/app/slide/slide.component.ts
+++ src/app/slide/slide.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-slide',
   templateUrl: './slide.component.html',
-  styleUrls: ['./slide.component.css']
+  styleUrls: ['./slide.component.css',
+              '../css/global.css',
+              '../css/easy-responsive-tabs.css',
+              '../css/slider.css',
+              '../css/style.css',]
 })
 export class SlideComponent implements OnInit {
 
